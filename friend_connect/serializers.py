f1f707b1cb944b3a59c98155796293fc98294ed1
Gaurav Saini <gaurav.c.saini@ericsson.com>
2024-05-16 16:09:39 +0530

    further changes

--- friend_connect/serializers.py
+++ friend_connect/serializers.py
@@ -3,6 +3,9 @@ from .models import FriendConnect
 
 
 class FriendConnectSerializer(serializers.ModelSerializer):
+    sender_email = serializers.EmailField(source='sender.email', read_only=True)
+    receiver_email = serializers.EmailField(source='receiver.email', read_only=True)
+    
     class Meta:
         model = FriendConnect
-        fields = ['id', 'sender', 'receiver', 'status']
+        fields = ['id', 'sender', 'receiver', 'status', 'sender_email', 'receiver_email']
